3c0406a99e2700d21b3e930bc709b9473f071bf2
asm <aaasm@pm.me>
2021-10-02 10:13:29 +0300

    Stage & prod

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -16,26 +16,11 @@ variable "db_disk_image" {
   description = "Disk image for reddit db"
   default     = "reddit-db-base"
 }
-variable "image_id" {
-  description = "Disk image"
-}
 variable "subnet_id" {
   description = "Subnet"
 }
-variable "external_app_port" {
-  default = 8080
-}
-variable "internal_app_port" {
-  default = 9292
-}
-variable "app_instances_count" {
-  default = 1
-}
 variable "public_key_path" {
   description = "Path to the public key used for ssh access"
-}
-variable "private_key_path" {
-  description = "Path to the private key used for ssh access"
 }
 variable "service_account_key_file" {
   description = "key .json"
