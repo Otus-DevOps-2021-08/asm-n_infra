4b80e44972406d015d0bbdd80d5c230dff391b68
asm <aaasm@pm.me>
2021-10-05 13:24:09 +0300

    Fixes

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,11 +1,4 @@
 terraform {
   backend "s3" {
-    endpoint                    = "storage.yandexcloud.net"
-    bucket                      = "app-storage"
-    region                      = "us-east-1"
-    key                         = "app.tfstate"
-    access_key                  = "W436Lo1Zn0H-sZX3RGEO"
-    skip_region_validation      = true
-    skip_credentials_validation = true
   }
 }
---
+++ terraform/storage-backend.tf
@@ -0,0 +1,7 @@
+endpoint                    = "storage.yandexcloud.net"
+bucket                      = "app-storage"
+region                      = "us-east-1"
+key                         = "app.tfstate"
+access_key                  = "W436Lo1Zn0H-sZX3RGEO"
+skip_region_validation      = true
+skip_credentials_validation = true
