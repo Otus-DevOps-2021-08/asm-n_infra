c948dfdd19462329f324f4a87311e235d5cc1b99
asm <aaasm@pm.me>
2021-10-05 14:56:48 +0300

    Add module 'vpc'

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -15,7 +15,7 @@ module "app" {
   source           = "../modules/app"
   public_key_path  = var.public_key_path
   app_disk_image   = var.app_disk_image
-  subnet_id        = var.subnet_id
+  subnet_id        = module.vpc.app_subnet_id
   private_key_path = var.private_key_path
   database_url     = module.db.internal_ip_address_db
 }
@@ -24,6 +24,10 @@ module "db" {
   source           = "../modules/db"
   public_key_path  = var.public_key_path
   db_disk_image    = var.db_disk_image
-  subnet_id        = var.subnet_id
+  subnet_id        = module.vpc.app_subnet_id
   private_key_path = var.private_key_path
+}
+
+module "vpc" {
+  source = "../modules/vpc"
 }
